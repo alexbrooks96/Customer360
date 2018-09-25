a56f411e6c0eb603e0610e01d2ff1e539e44f285
S0019840484 <alex.brooks@delaware.co.uk>
2018-09-25 09:41:45 +0000

    Fourth commit

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -5,6 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("com.delaware.ab.trac2018.controller.Master", {
 
+
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
@@ -28,9 +29,21 @@ sap.ui.define([
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf com.delaware.ab.trac2018.view.Master
 		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+			onAfterRendering: function() {
+		var oModel = this.getView().getModel();
+		var that = this;
+		
+			oModel.read("/ZV_ZVT18_CUSTM_JVN", {
+			success: function(oData){
+				that.getView().getModel("customersModel").setData({
+					"customers": oData.results
+				});
+			},
+			error: function(oError){
+				console.log(oError);
+			}
+		});
+	}
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
